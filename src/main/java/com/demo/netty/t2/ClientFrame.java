eb40ff176083fd1a4fc7f91dd0b0551c15d441f9
wuxiaomeng94 <806620602@qq.com>
2019-12-14 15:24:12 +0800

    talk frame

---
+++ src/main/java/com/demo/netty/t2/ClientFrame.java
@@ -0,0 +1,33 @@
+package com.demo.netty.t2;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClientFrame extends Frame {
+
+    TextArea ta = new TextArea();
+    TextField tf = new TextField();
+
+    public ClientFrame() {
+        this.setSize(600, 400);
+        this.setLocation(100, 20);
+        this.add(ta, BorderLayout.CENTER);
+        this.add(tf, BorderLayout.SOUTH);
+
+        tf.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //在这把字符串发送到服务器
+                ta.setText(ta.getText() + tf.getText());
+                tf.setText("");
+            }
+        });
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ClientFrame();
+    }
+
+}
